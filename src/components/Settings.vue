e3c9c58267422bc302b7a0c83ce849d130fd9b52
Stefan Fernandez <stefan.fernandez@gmail.com>
2021-01-09 16:25:50 -0500

    Update backup format

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -51,7 +51,7 @@
           class="mb-2"
           small
           :disabled="!backupFileParsed"
-          @click="$store.dispatch('commitBulkDocsToPouchAndVuex', backupFileParsed)"
+          @click="$store.dispatch('commitBulkDocsToPouchAndVuex', backupFileParsed.rows)"
         >
           Restore From File
         </v-btn>
@@ -208,18 +208,15 @@ export default {
     onFileChange() {
       console.log(this.backupFile)
 
-            const reader = new FileReader();
+      const reader = new FileReader();
       this.accountsForImport = [];
       this.selectedAccount = {};
 
       reader.onload = e => {
         const vm = this;
         let data = JSON.parse(e.target.result)
-        let rowsForImport = data.rows.map(row => row.doc).map(row => {
-          row._rev = null
-          return row
-        });
-        vm.backupFileParsed = rowsForImport
+
+        vm.backupFileParsed = data
       };
       reader.readAsText(this.backupFile);
     },
