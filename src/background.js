abc7308b1355ef2c7ff97ca9301e8145d41ee20a
Stefan Fernandez <stefan.fernandez@gmail.com>
2021-02-20 18:20:35 -1000

    Update devtools

--- src/background.js
+++ src/background.js
@@ -1,8 +1,9 @@
 import { app, protocol, BrowserWindow } from "electron";
 import { autoUpdater } from "electron-updater";
-import { createProtocol, installVueDevtools } from "vue-cli-plugin-electron-builder/lib";
+import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
 import path from "path";
 import url from "url";
+import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
 
 const isDevelopment = process.env.NODE_ENV !== "production";
 
@@ -79,7 +80,7 @@ app.on("ready", async () => {
     // If you are not using Windows 10 dark mode, you may uncomment these lines
     // In addition, if the linked issue is closed, you can upgrade electron and uncomment these lines
     try {
-      await installVueDevtools();
+      await installExtension(VUEJS_DEVTOOLS)
     } catch (e) {
       console.error("Vue Devtools failed to install:", e.toString());
     }
