fc4b941a26e29a8b3b869442578779e4e616fd97
Stefan Fernandez <stefan.fernandez@gmail.com>
2021-04-22 00:40:46 +0000

    add date range

--- src/components/Reports.vue
+++ src/components/Reports.vue
@@ -4,6 +4,33 @@
       <span class="text-h3 pt-4">Reports</span>
       <v-divider class="pb-4" />
 
+      <v-row>
+        <v-col>
+          <v-menu
+          v-model="menu2"
+          :close-on-content-click="false"
+          :nudge-right="40"
+          transition="scale-transition"
+          offset-y
+          min-width="auto"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+              v-model="date"
+              :label=dateRangeText
+              prepend-icon="mdi-calendar"
+              readonly
+              v-bind="attrs"
+              v-on="on"
+            ></v-text-field>
+          </template>
+                    <v-date-picker v-model="dateRange" range @input="dateMenu = false" type="month" /> 
+
+        </v-menu>
+
+          </v-col
+      ></v-row>
+
       <div class="pa-5">
         <line-chart :chart-data.sync="incomeAndSpentByMonth" />
       </div>
@@ -22,10 +49,14 @@ export default {
   },
   data() {
     return {
+      dateRange: []
     };
   },
   computed: {
-    ...mapGetters(["incomeAndSpentByMonth"])
+    ...mapGetters(["incomeAndSpentByMonth"]),
+    dateRangeText() {
+      return this.dateRange.join(' - ')
+    }
   },
   watch: {},
   mounted() {},
