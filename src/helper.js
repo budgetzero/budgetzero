5d5f6e3e67d2825445f59f375c16581fad75d714
Stefan Fernandez <stefan.fernandez@gmail.com>
2021-05-23 12:46:46 -0400

    Don't strip negative sign from amount

--- src/helper.js
+++ src/helper.js
@@ -1,6 +1,6 @@
 // Clean/sanitize amount input
 function sanitizeValueInput(value) {
-  let amt = value.toString().replace(/[^0-9.]/g, '')
+  let amt = value.toString().replace(/[^0-9.-]/g, '')
   return parseInt(amt)
 }
 
