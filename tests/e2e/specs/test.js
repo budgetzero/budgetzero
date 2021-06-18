d5b01d8a5267e6fa3ecd387831ca2425aef6e5f8
Stefan Fernandez <stefan.fernandez@gmail.com>
2021-06-17 21:51:03 -0400

    Add a few e2e tests

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -26,8 +26,34 @@ describe('My First Test', () => {
 
   it('Add a transaction', () => {
     cy.get('#addTransactionBtn').click()
-      
-   
+    
+    cy.get('#inflow-input').type('55.55')
+    cy.get('#save-btn').click()
+
+    cy.get(".transaction-table tbody").find("tr").should("have.length", 1);
+    cy.get("#inflow").should("contain", "55.55");
   })
+
+  it('Add a transaction 2', () => {
+    cy.get('#addTransactionBtn').click()
+    
+    cy.get('#outflow-input').type('66.55')
+    cy.get('#save-btn').click()
+
+    cy.get(".transaction-table tbody").find("tr").should("have.length", 2);
+    cy.get(".transaction-table tbody").should("contain", "66.55");
+  })
+
+  it('test malformed', () => {
+    cy.get('#addTransactionBtn').click()
+    
+    cy.get('#outflow-input').type('7ff5.g58')
+    cy.get('#save-btn').click()
+
+    cy.get(".transaction-table tbody").find("tr").should("have.length", 3);
+    cy.get(".transaction-table tbody").should("contain", "75.58");
+  })
+
+  
 })
 
