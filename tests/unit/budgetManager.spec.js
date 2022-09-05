c8e4874abf2c14795e6a2f1596ef2835cd91ad8b
Stefan Fernandez <stefan.fernandez@gmail.com>
2022-09-05 15:35:08 -0400

    fix unit test

--- tests/unit/budgetManager.spec.js
+++ tests/unit/budgetManager.spec.js
@@ -1,27 +1,20 @@
-// stores/counter.spec.ts
-import { setActivePinia, createPinia } from 'pinia'
-import { useBudgetManagerStore } from '../../src/store/budgetManager'
-import { useBudgetHelperStore } from '../../src/store/budgetManagerHelper'
-import { usePouchDBStore } from '../../src/store/pouchdbStore'
-
-import validator from 'validator'
-import { v4 as uuidv4 } from 'uuid'
-
+import { budgetManager } from '../../src/store/budgetManager'
 import mock_budget from '@/../tests/__mockdata__/mock_budget2.json'
 import PouchDB from 'pouchdb'
 
-let budgetmanager
-let budgetHelper
-let pouchdbStore
+new PouchDB('budgetzero_local_db12333')
+  .destroy()
+  .then(function () {
+    // database destroyed
+  })
+  .catch(function (err) {
+    // error occurred
+  })
+let budgetmanager = new budgetManager()
 
-describe('budget manager', () => {
-  beforeEach(async () => {
-    // creates a fresh pinia and make it active so it's automatically picked
-    // up by any useStore() call without having to pass it to it:
-    // `useStore(pinia)`
-    setActivePinia(createPinia())
-    budgetmanager = useBudgetManagerStore()
-    await budgetmanager.loadMockDataIntoPouchDB(mock_budget, '5a98dc44-7982-4ecc-aa50-146fc4dc4e16')
+describe('budget-manager', () => {
+  beforeAll(async () => {
+   await budgetmanager.loadMockDataIntoPouchDB(mock_budget, '5a98dc44-7982-4ecc-aa50-146fc4dc4e16')
   })
 
   it('calculates correctly', async () => {
@@ -31,7 +24,7 @@ describe('budget manager', () => {
 
   it('loaded the data', async () => {
     // Subtract budget and budget-opened docs
-    expect(budgetmanager.budgetData.length).toBe(620)
+    expect(budgetmanager.budgetData.length).toBe(mock_budget.length-2)
   })
 
   it('has correct number of transactions', async () => {
@@ -56,13 +49,7 @@ describe('budget manager', () => {
 })
 
 describe('budget-manager transactions', () => {
-  beforeEach(async () => {
-    // creates a fresh pinia and make it active so it's automatically picked
-    // up by any useStore() call without having to pass it to it:
-    // `useStore(pinia)`
-    setActivePinia(createPinia())
-    budgetmanager = useBudgetManagerStore()
-    pouchdbStore = usePouchDBStore()
+  beforeAll(async () => {
     await budgetmanager.loadMockDataIntoPouchDB(mock_budget, '5a98dc44-7982-4ecc-aa50-146fc4dc4e16')
   })
 
@@ -107,16 +94,16 @@ describe('budget-manager transactions', () => {
   })
 
   it('get and modify transaction', async () => {
-    const original_transaction = await pouchdbStore.localdb.get(
+    const original_transaction = await budgetmanager.pouchdbManager.localdb.get(
       'b_5a98dc44-7982-4ecc-aa50-146fc4dc4e16_transaction_2b84300c-9503-497e-be96-dfc9a683027f'
     )
     original_transaction.name = 'new name'
 
     const num_of_trans = budgetmanager.transactions.length
     const num_of_docs = budgetmanager.budgetData.length
-
+    
     let resp = await budgetmanager.putDocument(original_transaction)
-
+      
     // Number of data shouldn't change for a modification
     expect(budgetmanager.transactions.length).toBe(num_of_trans)
     expect(budgetmanager.budgetData.length).toBe(num_of_docs)
@@ -124,14 +111,14 @@ describe('budget-manager transactions', () => {
     expect(resp['ok']).toBe(true)
 
     //Check that it was modified
-    const updated_transaction = await pouchdbStore.localdb.get(
+    const updated_transaction = await budgetmanager.pouchdbManager.localdb.get(
       'b_5a98dc44-7982-4ecc-aa50-146fc4dc4e16_transaction_2b84300c-9503-497e-be96-dfc9a683027f'
     )
     expect(updated_transaction.name).toBe('new name')
   })
 
   it('get and modify malformed transaction', async () => {
-    const original_transaction = await pouchdbStore.localdb.get(
+    const original_transaction = await budgetmanager.pouchdbManager.localdb.get(
       'b_5a98dc44-7982-4ecc-aa50-146fc4dc4e16_transaction_2b84300c-9503-497e-be96-dfc9a683027f'
     )
     original_transaction.value = 'not a number'
@@ -140,11 +127,13 @@ describe('budget-manager transactions', () => {
     await expect(budgetmanager.putDocument(original_transaction)).rejects.toThrowError('Document failed validation')
     expect(budgetmanager.transactions.length).toBe(num_of_trans)
   })
+
+    
 })
 
 describe('budget-manager monthlyData calculates when', () => {
   beforeAll(async () => {
-    await budgetmanager.loadMockDataIntoPouchDB(mock_budget, '5a98dc44-7982-4ecc-aa50-146fc4dc4e16')
+   await budgetmanager.loadMockDataIntoPouchDB(mock_budget, '5a98dc44-7982-4ecc-aa50-146fc4dc4e16')
   })
 
   it('add transaction', async () => {
@@ -171,15 +160,104 @@ describe('budget-manager monthlyData calculates when', () => {
 
   it('modify budget amount', async () => {
     // const data = budgetmanager.monthlyData
-    let resp = await budgetmanager.putDocument({
-      budget: 20000,
-      overspending: null,
-      note: '',
-      _id: 'b_5a98dc44-7982-4ecc-aa50-146fc4dc4e16_m_category_2019-08-01_02ec642d-25c6-4f4b-a21f-3b4a5b4025c1'
+    let resp = await budgetmanager.putDocument(  {
+      "budget": 20000,
+      "overspending": null,
+      "note": "",
+      "_id": "b_5a98dc44-7982-4ecc-aa50-146fc4dc4e16_m_category_2019-08-01_02ec642d-25c6-4f4b-a21f-3b4a5b4025c1"
     })
     expect(budgetmanager.monthlyData).toMatchSnapshot()
     // const after = budgetmanager.monthlyData
     // expect(data).toStrictEqual(after)
   })
+
+ 
 })
 
+describe('budget-manager putBulkDocuments', () => {
+  beforeAll(async () => {
+   await budgetmanager.loadMockDataIntoPouchDB(mock_budget, '5a98dc44-7982-4ecc-aa50-146fc4dc4e16')
+  })
+
+  it('add bulk transaction', async () => {
+
+    const num_of_trans = budgetmanager.transactions.length
+    const num_of_docs = budgetmanager.budgetData.length
+    
+    const bulk = [{
+      account: '38e690f8-198f-4735-96fb-3a2ab15081c2',
+      category: null,
+      cleared: false,
+      approved: false,
+      value: -100000,
+      date: '2015-05-10',
+      memo: 'memo',
+      reconciled: false,
+      flag: '#ffffff',
+      payee: 'c28737d0-1519-4c47-a718-9bda6df392fc',
+      transfer: null,
+      splits: [],
+      _id: 'b_5a98dc44-7982-4ecc-aa50-146fc4dc4e16_transaction_31a2483b-d0e5-4daf-b1fe-f1788ed0yyyy'
+    },{
+      account: '38e690f8-198f-4735-96fb-3a2ab15081c2',
+      category: null,
+      cleared: false,
+      approved: false,
+      value: -150000,
+      date: '2015-05-10',
+      memo: 'memo',
+      reconciled: false,
+      flag: '#ffffff',
+      payee: 'c28737d0-1519-4c47-a718-9bda6df392fc',
+      transfer: null,
+      splits: [],
+      _id: 'b_5a98dc44-7982-4ecc-aa50-146fc4dc4e16_transaction_31a2483b-d0e5-4daf-b1fe-f1788ed0zzzz'
+      }]
+    
+    let resp = await budgetmanager.putBulkDocuments(bulk)
+    
+    // Number of data shouldn't change for a modification
+    expect(budgetmanager.transactions.length).toBe(num_of_trans + bulk.length)
+    expect(budgetmanager.budgetData.length).toBe(num_of_docs + bulk.length)
+  })
+
+  it('add bad transaction', async () => {
+    await expect(
+      budgetmanager.putBulkDocuments([{
+        category: null,
+        cleared: false,
+        approved: false,
+        value: -4444,
+        date: '2015-05-10',
+        memo: 'memo',
+        reconciled: false,
+        flag: '#ffffff',
+        payee: 'c28737d0-1519-4c47-a718-9bda6df392fc',
+        transfer: null,
+        splits: [],
+        _id: 'b_5a98dc44-7982-4ecc-aa50-146fc4dc4e16_transaction_31a2483b-d0e5-4daf-b1fe-f1788ed01234'
+      }])
+    ).rejects.toThrowError('Document failed validation')
+  })
+
+  it('add bad transaction with wrong length ID', async () => {
+    await expect(
+      budgetmanager.putBulkDocuments([{
+        account: '38e690f8-198f-4735-96fb-3a2ab15081c2',
+        category: null,
+        cleared: false,
+        approved: false,
+        value: -4444,
+        date: '2015-05-10',
+        memo: 'memo',
+        reconciled: false,
+        flag: '#ffffff',
+        payee: 'c28737d0-1519-4c47-a718-9bda6df392fc',
+        transfer: null,
+        splits: [],
+        _id: 'b_5a98dc44-7982-4ecc-aa50-146fc4dc4e16_transaction_31a2483b-d0e5-4da'
+      }])
+    ).rejects.toThrowError('Document failed validation')
+  })
+
+})
